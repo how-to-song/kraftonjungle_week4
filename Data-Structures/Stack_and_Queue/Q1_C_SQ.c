2c03d9af3f946d463de148fba03e3927f52a76ed
송하우 <song03621@gmail.com>
2026-09-10 22:03:24 +0900

    Q1_C_SQ 코드 초안

--- Data-Structures/Stack_and_Queue/Q1_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q1_C_SQ.c
@@ -117,11 +117,28 @@ int main()
 void createQueueFromLinkedList(LinkedList *ll, Queue *q)
 {
 	/* add your code here */
+	ListNode *temp = ll->head;
+	while (temp != NULL){
+		enqueue(q, temp->item);
+		temp = temp->next;
+	}
 }
 
 void removeOddValues(Queue *q)
 {
 	/* add your code here */
+	ListNode *curr = (q->ll).head;
+	int len = (q->ll).size;
+	
+	for (int i = 0; i < len; i++) {
+		int temp = dequeue(q);
+
+		if (temp % 2 == 0) {
+			enqueue(q, temp);
+		}
+		
+		curr = curr->next;
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
