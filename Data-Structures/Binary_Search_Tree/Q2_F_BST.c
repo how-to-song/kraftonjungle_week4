6d8198d68181ad0e6776fb118423db50b298f59c
송하우 <song03621@gmail.com>
2026-09-14 13:04:23 +0900

    Q2_F_BST 구현 closes #32

--- Data-Structures/Binary_Search_Tree/Q2_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q2_F_BST.c
@@ -90,7 +90,21 @@ int main()
 
 void inOrderTraversal(BSTNode *root)
 {
-	 /* add your code here */
+	/* add your code here */
+	Stack s = { NULL };
+	BSTNode *curr = root;
+
+	while(curr != NULL || !isEmpty(&s)) {
+		while (curr != NULL) {
+			push(&s, curr);
+			curr = curr->left;
+		}
+
+		curr = pop(&s);
+		printf("%d ", curr->item);
+
+		curr = curr->right;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
