1db4907d5b29c5d7bf30065028ebe08cf132bc4e
송하우 <song03621@gmail.com>
2026-09-14 11:09:52 +0900

    Q1_F_BST 구현 closes #31

--- Data-Structures/Binary_Search_Tree/Q1_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q1_F_BST.c
@@ -93,8 +93,20 @@ int main()
 
 void levelOrderTraversal(BSTNode* root)
 {
-
     /* add your code here */
+	if(root == NULL) {
+		return;
+	}
+
+	Queue queue = { NULL, NULL };
+	enqueue(&queue.head, &queue.tail, root);
+
+	while (!isEmpty(queue.head)) {
+		BSTNode *node = dequeue(&queue.head, &queue.tail);
+		printf("%d ", node->item);
+		if(node->left != NULL) enqueue(&queue.head, &queue.tail, node->left);
+		if(node->right != NULL) enqueue(&queue.head, &queue.tail, node->right);
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
